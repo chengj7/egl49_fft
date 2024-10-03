7ec774794ed2e519d8ba4ec0cd97c109d98b6f02
egl49 <egl49@cornell.edu>
2024-10-03 13:09:40 -0400

    updated code

--- mechanical_vocal_cord_code.py
+++ mechanical_vocal_cord_code.py
@@ -1,3 +1,4 @@
+""" based off of this tutorial: https://apmonitor.com/dde/index.php/Main/AudioAnalysis """
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
@@ -5,3 +6,20 @@ from scipy import signal
 from pydub import AudioSegment
 
 f1 = 
+#analyze entire audio clip
+na = len(a)
+a_k = np.fft.fft(a)[0:int(na/2)]/na # FFT function from numpy
+a_k[1:] = 2*a_k[1:] # single-sided spectrum only
+Pxx = np.abs(a_k)   # remove imaginary part
+f = s*np.arange((na/2))/na # frequency vector
+
+#plotting
+fig,ax = plt.subplots()
+plt.plot(f,Pxx,'b-',label='Audio Signal')
+plt.plot([20,20000],[0.1,0.1],'r-',alpha=0.7,\
+         linewidth=10,label='Audible (Humans)')
+ax.set_xscale('log'); ax.set_yscale('log')
+plt.grid(); plt.legend()
+plt.ylabel('Amplitude')
+plt.xlabel('Frequency (Hz)')
+plt.show()
